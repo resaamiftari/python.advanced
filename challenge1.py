e53045b5394381a66e1ea4ff4ef84d9a080fa13d
resaamiftari <resqmiftari4@icloud.com>
2026-04-29 21:38:16 -0700

    .

--- challenge1.py
+++ challenge1.py
@@ -10,9 +10,25 @@ informacionet1 = {
     "email": "jane@gmail.com"
     }
 
-print(informacionet.keys())
-print(informacionet.values())
 
+print(informacionet)
+print(informacionet1)
+
+# Marrja e një vlere
+email = informacionet["email"]
+print(email)
+
+# Shtimi i një fushe të re
+informacionet["age"] = 20
+print(informacionet)
+
+# Ndryshimi i një vlere
+informacionet1["phone"] = "111111111"
+print(informacionet1)
+
+# Fshirja e një fushe
+del informacionet["email"]
+print(informacionet)
 
 
 
