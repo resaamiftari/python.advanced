1083facac96bd8a9128c81a92efcac87e1a23ede
resaamiftari <resqmiftari4@icloud.com>
2026-09-17 20:34:12 +0200

    lesson20

---
+++ PythonProjectlesson20/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+app=FastAPI()
+
+@app.get("/")
+
+def root():
+    return {
+        "address": {
+            "street": "pashko vasa",
+            "city": "prishtine",
+            "country": "kosove"
+
+        },
+        "contacts": [
+            {
+                "type": "email",
+                "value": "donjeta@gmail.com"
+            },
+            {
+                "type": "phone",
+                "value": "555-123-4567"
+            }
+        ]
+    }
